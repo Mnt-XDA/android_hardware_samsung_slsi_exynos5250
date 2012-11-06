a26fc6b3a6f593141b694ca642765d3d2c2dd358
meka rama <v.meka@samsung.com>
2012-11-06 10:18:49 +0900

    libhwcmodule: Added an initial code for hwc module
    Change-Id: Ib67b8a9dfb9a6be323d5cd31a8c1906b72bca9b1
    Signed-off-by: meka rama <v.meka@samsung.com>

---
+++ libhwcmodule/ExynosHWCModule.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANDROID_EXYNOS_HWC_MODULE_H_
+#define ANDROID_EXYNOS_HWC_MODULE_H_
+#define EXYNOS_HDMI_DISPLAY
+const size_t GSC_DST_W_ALIGNMENT_RGB888 = 32;
+const size_t GSC_DST_CROP_W_ALIGNMENT_RGB888 = 32;
+#define VSYNC_DEV_NAME  "/sys/devices/platform/exynos5-fb.1/vsync"
+#endif
